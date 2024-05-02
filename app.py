6098540bbb23ed6eed800b77a48fcde61c185952
Kiran Gupta <kiran.rgupta26@gmail.com>
2024-05-02 16:02:34 +0530

    Update to Flask App

--- app.py
+++ app.py
@@ -7,31 +7,35 @@ from models import StockInput
 from DataHandler import DataHandler
 from StockData import StockData
 
-#app = flask.Flask(__name__)
-app =  FastAPI()
+app = flask.Flask(__name__)
+#app =  FastAPI()
 
-# @app.route('/')
-# def hello():
-#     msg = "Docker Works"
-#     return msg
+@app.route('/')
+def hello():
+    msg = "Docker Works"
+    return msg
 
 #App routing
 @app.post('/pattern/stock')
-async def getstockpattern(inp:StockInput):
-    ticker = inp.ticker
-    api_key = "c102ba52a69f4c859f231c77c9ddb198"
-    timespan = inp.time
+def getstockpattern():
+#async def getstockpattern(inp:StockInput):
+    # ticker = inp.ticker
+    # api_key = "c102ba52a69f4c859f231c77c9ddb198"
+    # timespan = inp.time
 
-    #data = request.get_json()
-    #print("Json Data ",data["ticker"])
+    data = request.get_json()
+    ticker = data["ticker"]
+    api_key = "c102ba52a69f4c859f231c77c9ddb198"
+    timespan = data["time"]
+
     data_handler = DataHandler()
     results = data_handler.get_stock_prices(ticker, api_key,timespan)
     print("Flask Appications")
     return results
 
 if __name__ == "__main__":
-    #app.run(host = "0.0.0.0",debug=True)
-    uvicorn.run(app,host = "0.0.0.0",port=5000)
+    app.run(host = "0.0.0.0",debug=True)
+    #uvicorn.run(app,host = "0.0.0.0",port=5000)
 
 
 
